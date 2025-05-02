e385b7a18c228fd7f3690888b9f74f345cb19461
sharmaketan03 <ketan301024@gmail.com>
2025-05-02 10:39:22 +0530

    add languages

--- index.js
+++ index.js
@@ -3,10 +3,34 @@ let source_lang=document.querySelector("#select1")
 let target_lang=document.querySelector("#select2")
 let button=document.querySelector("button")
 let results=document.querySelector("#results")
+async function getlanguage(){
+    let url = "https://text-translator2.p.rapidapi.com/getLanguages";
+    const headers={
+         "x-rapidapi-key": "fcc2337bc8msh42ed8779cd4654cp181e15jsn3854dedb9b02",
+        "x-rapidapi-host": "text-translator2.p.rapidapi.com"
+    }
+    let response=await fetch(url,{
+        method:"get",
+        headers:headers
+    })
+    let result=await response.json();
+    // console.log(result.data)
+    getlanguagecodes(result.data)
+}
+getlanguage()
 
-
-
-
+function  getlanguagecodes(obj){
+    obj.languages.forEach((element) => {
+           let option=document.createElement("option")
+           option.value=element.code
+           option.innerHTML=element.name
+           source_lang.append(option)
+           let option1=document.createElement("option")
+           option1.value=element.code
+           option1.innerHTML=element.name
+           target_lang.append(option1)
+    });
+}
 
 
 
